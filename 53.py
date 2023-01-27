e56af8ade6cfa6f92ce78aacc8b5b597829614b8
Mouly <abiraazmary22@gmail.com>
2023-01-28 00:23:26 +0600

    .

---
+++ 53.py
@@ -0,0 +1,35 @@
+def righttriangle(number, inc):
+    suum = number+ inc
+    if number <= 0:
+        return 1
+    else:
+        blankspace(number - 1)
+        increment(inc,number,suum)
+        righttriangle(number - 1, inc+1)
+        
+def blankspace(s_count):
+    if s_count == 0:
+        return
+    else:
+        print(" ", end = ' ')
+        blankspace(s_count - 1)
+
+    
+def increment(count,number,suum):
+    if count <= 0:
+        return 
+    else:
+        increment(count - 1, number,suum)
+        if count+number == suum:
+            print(count)
+        else:
+            print(count, end = ' ')
+righttriangle(5, 1)
+
+
+#output:
+#        1
+#      1 2
+#    1 2 3
+#  1 2 3 4
+#1 2 3 4 5
